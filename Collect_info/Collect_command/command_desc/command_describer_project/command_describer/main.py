1ec90feff9eb0c0f65c33f7b9788d1454bf288c5
AidanAcartis <ralaikotonadia@gmail.com>
2026-03-27 22:11:33 +0300

    Reorganized taskMonitor

--- Collect_info/Collect_command/command_desc/command_describer_project/command_describer/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from command_describer.core.describer import CommandDescriber
-
-def main():
-    describer = CommandDescriber()
-    describer.run()
-
-if __name__ == "__main__":
-    main()
